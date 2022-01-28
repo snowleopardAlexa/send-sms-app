c3cc4cf6b832c369b43ab3f7a86bf98b7552dc0f
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-27 21:24:47 -0500

    add function send sms, send a request to the endpoint, recieve the request

--- src/App.js
+++ src/App.js
@@ -7,11 +7,38 @@ function App() {
 const [number, setNumber] = useState('');
 const [body, setBody] = useState('');
 
+// function send SMS to the number submitted through the form
+const onSubmit = async (e) => {
+  await e.preventDefault();
+
+  const res = await fetch("/api/sendMessage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ to: number, body: body}),
+  });
+
+  // after the request, the response from the endpoint is saved in the data variable
+  const data = await res.json();
+
+  if (data.success) {
+    await setNumber("");
+    await setBody("");
+  } else {
+    await setNumber("An error has occured.");
+    await setBody("An error has occured.")
+  }
+}
+
   return (
     <div className="App">
       <Container>
         <h2>Send SMS</h2>
-        <Form className="mt-4">
+        <Form 
+          className="mt-4"
+          onSubmit={onSubmit}
+        >
           <Form.Group>
             <Form.Label htmlFor="to">To</Form.Label>
             <Form.Control 
