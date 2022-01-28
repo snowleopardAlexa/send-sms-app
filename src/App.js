76f833fa37d5677d8840434b8ff571ef6be06221
snowleopardAlexa <alexaslomski@gmail.com>
2022-01-27 20:52:50 -0500

    add btn to form control

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import { useState } from 'react';
 import "./App.css";
 import { Container, Form, Button } from "react-bootstrap";
 
 function App() {
+
+const [number, setNumber] = useState('');
+const [body, setBody] = useState('');
+
   return (
     <div className="App">
       <Container>
         <h2>Send SMS</h2>
-        <Form>
+        <Form className="mt-4">
           <Form.Group>
             <Form.Label htmlFor="to">To</Form.Label>
-            <Form.Control />
+            <Form.Control 
+              value={number} 
+              onChange={(e) => setNumber(e.target.value)} 
+            />
           </Form.Group>
 
           <Form.Group>
             <Form.Label htmlFor="message">Body</Form.Label>
-            <Form.Control as="textarea" rows="3" />
+            <Form.Control 
+              as="textarea" 
+              rows="3" 
+              onChange={(e) => setBody(e.target.value)}
+            />
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button className="mt-4" variant="primary" type="submit">
             Send
           </Button>
         </Form>
